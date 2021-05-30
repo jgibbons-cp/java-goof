137f351fc7a19184b7f2e6cbdc397d2cf3e4d9f9
Jenks Gibbons <jenksgibbons@gmail.com>
2021-05-30 04:25:17 -0700

    dev

--- env_vars.sh
+++ env_vars.sh
@@ -2,4 +2,4 @@ export DATADOG_APP_KEY=
 export DATADOG_API_KEY=
 export SNYK_TOKEN=
 export MAVEN_OPTS="-javaagent:dd-java-agent.jar -Ddd.profiling.enabled=true -XX:FlightRecorderOptions=stackdepth=256 -Ddd.service=javagoof -Ddd.env=lab -Ddd.version=.01"
-export PATH=:$PATH:/home/ec2-user/java-goof/aphe-maven-3.8.1/bin/
+export PATH=:$PATH:/home/ec2-user/java-goof/apache-maven-3.8.1/bin/
