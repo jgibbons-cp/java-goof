8e9bbd478446bf045dad738cd6ac9f193c61d174
Jenks Gibbons <jenksgibbons@gmail.com>
2021-05-30 05:31:51 -0700

    dev

--- setup.sh
+++ setup.sh
@@ -36,4 +36,5 @@ node_modules/.bin/datadog-ci dependencies upload deps.json --source snyk --servi
 wget -O dd-java-agent.jar https://dtdg.co/latest-java-tracer
 
 #let's go
+mvn install
 mvn tomcat7:run
