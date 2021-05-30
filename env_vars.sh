c9e0939c2a1530ab9b6e8362eb8cf905f77cfe59
Jenks Gibbons <jenksgibbons@gmail.com>
2021-05-30 03:35:09 -0700

    dev

--- env_vars.sh
+++ env_vars.sh
@@ -1,4 +1,4 @@
-DATADOG_APP_KEY=
-DATADOG_API_KEY=
-SNYK_TOKEN=
+export DATADOG_APP_KEY=
+export DATADOG_API_KEY=
+export SNYK_TOKEN=
 export MAVEN_OPTS="-javaagent:dd-java-agent.jar -Ddd.profiling.enabled=true -XX:FlightRecorderOptions=stackdepth=256 -Ddd.service=javagoof -Ddd.env=lab -Ddd.version=.01"
